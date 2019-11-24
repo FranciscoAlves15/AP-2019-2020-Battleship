bdf87dcc60c03cf17da37628c400bc4bd0ad156c
André Sabino <andre.sabino@gmail.com>
2019-11-24 22:51:49 +0000

    Set stub command parser.

---
+++ program.py
@@ -0,0 +1,35 @@
+import controllers
+
+def main():
+    while True:
+        line = input()
+        if not line:
+            exit(0)
+        commands = line.split(" ")
+        if commands[0] == "RJ":
+            pass
+        elif commands[0] == "EJ":
+            pass
+        elif commands[0] == "LJ":
+            pass
+        elif commands[0] == "IL":
+            pass
+        elif commands[0] == "IC":
+            pass
+        elif commands[0] == "D":
+            pass
+        elif commands[0] == "CN":
+            pass
+        elif commands[0] == "RN":
+            pass
+        elif commands[0] == "T":
+            pass
+        elif commands[0] == "V":
+            pass
+        elif commands[0] == "G":
+            pass
+        elif commands[0] == "L":
+            pass
+
+if __name__ == "__main__":
+    main()
