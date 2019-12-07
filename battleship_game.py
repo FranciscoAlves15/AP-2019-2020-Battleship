3b139cfbcac257e663357b07a6b2499cce51e586
André Sabino <andre.sabino@gmail.com>
2019-12-07 14:55:06 +0000

    Set initial game state.

--- battleship_game.py
+++ battleship_game.py
@@ -1,7 +1,28 @@
 def new_game():
     return {
+        'ships': {
+            'L': {
+                'size': 1,
+                'quantity': 4
+            },
+            'S': {
+                'size': 2,
+                'quantity': 3
+            },
+            'F': {
+                'size': 3,
+                'quantity': 2
+            },
+            'C': {
+                'size': 4,
+                'quantity': 1
+            },
+            'P': {
+                'size': 5,
+                'quantity': 1
+            },
+        },
         'players': [],
-        'ships': [],
         'match': None
     }
 
@@ -18,22 +39,17 @@ def add_player(game, name):
         'wins': 0
     })
 
-def player_in_current_match(game, name):
+def player_in_match(game, name):
     if game['match']['first']['player']['name'] == name:
         return True
     else:
         return game['match']['second']['player']['name'] == name
 
 def remove_player(game, name):
-    for i in range(game['players']):
+    for i in range(len(game['players'])):
         if game['players'][i]['name'] == name:
             del game['players'][i]
             break
-
-def get_player(game, name):
-    for player in game['players']:
-        if player['name'] == name:
-            return player
 
 def has_players(game):
     return len(game['players']) != 0
@@ -44,19 +60,10 @@ def get_players(game):
 def has_match(game):
     return game['match'] is not None
 
-def start_match(game, player_1_name, player_2_name)
-    # TODO: Keep track of the placed ships.
+def start_match(game, player_1_name, player_2_name):
     game['match'] = {
-        'first': {
-            'player': get_player(game, player_1_name),
-            'ships': [[0 for _ in range(10)] for _ in range(10)],
-            'shots': [[0 for _ in range(10)] for _ in range(10)]
-        },
-        'second': {
-            'player': get_player(game, player_2_name),
-            'ships': [[0 for _ in range(10)] for _ in range(10)],
-            'shots': [[0 for _ in range(10)] for _ in range(10)]
-        } 
+        'first': __create_match_player(game, player_1_name),
+        'second': __create_match_player(game, player_2_name)
     }
 
 def all_ships_placed(game):
@@ -95,5 +102,37 @@ def is_valid_shot(game, line, column):
 def shot(game, player_name, line, column):
     pass
 
-def get_current_match_state(game):
+def get_match_state(game):
     pass
+
+def __get_num_ships(game):
+    return sum([ship['quantity'] for ship in game['ships'].values()])
+
+def __get_ship_names(game):
+    return [ship for ship in game['ships']]
+
+def __get_player(game, name):
+    for player in game['players']:
+        if player['name'] == name:
+            return player
+
+def __create_match_player(game, player_name):
+    return {
+        'player': __get_player(game, player_name),
+        'ships': {
+            'P': [],
+            'C': [],
+            'F': [],
+            'S': [],
+            'L': []
+        },
+        'boards': {
+            'ships': [[0 for _ in range(10)] for _ in range(10)],
+            'shots': [[0 for _ in range(10)] for _ in range(10)]
+        }
+    }
+
+if __name__ == "__main__":
+    game = new_game()
+    print(__get_num_ships(game))
+    print(__get_ship_names(game))
